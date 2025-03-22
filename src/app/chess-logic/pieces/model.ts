4424f836587aaac1e03928e53670cbe54777c8a3
Navaneeth_KB <63635668+NavaneethKB@users.noreply.github.com>
2025-03-22 12:16:39 +0530

    Project Initialisation

---
+++ src/app/chess-logic/pieces/model.ts
@@ -0,0 +1,25 @@
+export enum Color{
+    White,
+    Black
+}
+
+export type Coords={
+    x:number;
+    y:number;
+}
+
+
+export enum FENChar{
+    WhitePawn="P",
+    WhiteKnight="N",
+    WhiteBishop="B",
+    WhiteRook="R",
+    WhtieQueen="Q",
+    WhiteKing="K",
+    WhitePawn="P",
+    WhiteKnight="N",
+    WhiteBishop="B",
+    WhiteRook="R",
+    WhtieQueen="Q",
+    WhiteKing="K"
+}
